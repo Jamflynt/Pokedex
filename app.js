d03e08b2cfab627e79b1c5e96ab1d62ac6818cc3
Jamie Flynt <jamflynt@gmail.com>
2021-11-08 14:30:57 -0500

    Created getPokemonData function which makes a request to the PokeApi and executes the createPokeCard function using the response from the request as the argument for that function.

--- app.js
+++ app.js
@@ -17,3 +17,14 @@ function createPokeCard (pokemon) {
     <h3 class="name">${pokemon.data.name.toUpperCase()}</h3>
     `;
 }
+
+// 
+// 
+async function getPokemonData(id){
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const pokemonData = await axios.get(url);
+    console.log(pokemonData);
+    console.log(pokemonData.data.sprites.front_shiny);
+    console.log(pokemonData.data.name);
+    createPokeCard(pokemonData);
+}
